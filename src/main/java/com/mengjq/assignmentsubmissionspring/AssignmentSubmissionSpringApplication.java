457680649f9c457946ebb01e554221b637cddc8e
Junqingqing <gwills@163.com>
2022-07-13 15:42:49 +0800

    init

---
+++ src/main/java/com/mengjq/assignmentsubmissionspring/AssignmentSubmissionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mengjq.assignmentsubmissionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentSubmissionSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentSubmissionSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mengjq/assignmentsubmissionspring/AssignmentSubmissionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mengjq.assignmentsubmissionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmissionSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
