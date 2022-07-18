586053b80611a84f6d0960b67471be78df06be77
Junqingqing <gwills@163.com>
2022-07-18 13:16:08 +0800

    add basic design

--- src/main/java/com/mengjq/assignmentsubmission_spring/AssignmentSubmissionSpringApplication.java
+++ src/main/java/com/mengjq/assignmentsubmission_spring/AssignmentSubmissionSpringApplication.java
@@ -1,13 +1,21 @@
 package com.mengjq.assignmentsubmission_spring;
 
+import com.mengjq.assignmentsubmission_spring.demo.DemoApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class AssignmentSubmissionSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AssignmentSubmissionSpringApplication.class, args);
+
     }
 
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
