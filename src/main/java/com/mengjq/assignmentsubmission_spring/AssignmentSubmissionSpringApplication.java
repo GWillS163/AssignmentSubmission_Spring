a572a98b5b49946af1fa8df48ecdd9ca23312b51
junqing_Gitkraken <gwills@163.com>
2022-09-15 22:13:50 +0800

    version1 add sucessfull

--- src/main/java/com/mengjq/assignmentsubmission_spring/AssignmentSubmissionSpringApplication.java
+++ src/main/java/com/mengjq/assignmentsubmission_spring/AssignmentSubmissionSpringApplication.java
@@ -1,6 +1,5 @@
 package com.mengjq.assignmentsubmission_spring;
 
-import com.mengjq.assignmentsubmission_spring.demo.DemoApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
