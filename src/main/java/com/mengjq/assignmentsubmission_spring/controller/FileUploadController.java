8f9db2f783d8d60ce213d75c4afdd8d386655d07
KissesJun <gwills@163.com>
2023-03-02 16:59:28 +0800

    upload 上传功能完成

--- src/main/java/com/mengjq/assignmentsubmission_spring/controller/FileUploadController.java
+++ src/main/java/com/mengjq/assignmentsubmission_spring/controller/FileUploadController.java
@@ -1,18 +1,18 @@
 package com.mengjq.assignmentsubmission_spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.Objects;
 
 @RestController
 public class FileUploadController {
 
     @PostMapping("/upload")
-    public String up(String nickname, MultipartFile photo, HttpServletRequest request) {
+    public String up(String nickname, MultipartFile photo, HttpServletRequest request) throws IOException {
         System.out.println(nickname);
 
         System.out.println(photo.getOriginalFilename());
@@ -21,9 +21,21 @@ public class FileUploadController {
         System.out.println(photo.getName());
         System.out.println(System.getProperty("user.dir"));;
 
-        String path = request.getServletContext().getRealPath("/upload");
+        String path = request.getServletContext().getRealPath("/upload/");
         System.out.println(path);
+        saveFile(photo, path);
 
         return "upload success";
+    }
+
+    public void saveFile(MultipartFile photo, String path) throws IOException {
+        File dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        String savePath = path + photo.getOriginalFilename();
+        System.out.println("saving file to " + savePath);
+        File file = new File(savePath);
+        photo.transferTo(file);
     }
 }
