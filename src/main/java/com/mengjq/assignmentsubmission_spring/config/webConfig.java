b259f7a31eed69ed0ae8fb20e21206ad073ae8eb
KissesJun <gwills@163.com>
2023-03-02 17:33:23 +0800

    文件上传+拦截器ok

---
+++ src/main/java/com/mengjq/assignmentsubmission_spring/config/webConfig.java
@@ -0,0 +1,18 @@
+package com.mengjq.assignmentsubmission_spring.config;
+
+import com.mengjq.assignmentsubmission_spring.Interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/user/**")
+                .excludePathPatterns("/login", "/register", "/static/**", "/templates/**");
+        ;
+    }
+}
