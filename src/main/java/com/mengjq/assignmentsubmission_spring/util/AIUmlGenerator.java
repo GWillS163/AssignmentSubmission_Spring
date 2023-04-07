e033cf707c0f37f8ba66eabc2fbb5d55b6e6e006
KissesJun <gwills@163.com>
2023-04-07 13:58:52 +0800

    UML-AI 接口功能已实现并测试工程

--- src/main/java/com/mengjq/assignmentsubmission_spring/util/AIUmlGenerator.java
+++ src/main/java/com/mengjq/assignmentsubmission_spring/util/AIUmlGenerator.java
@@ -2,79 +2,157 @@ package com.mengjq.assignmentsubmission_spring.util;
 
 import com.alibaba.fastjson.JSONObject;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AIUmlGenerator {
     // set a property to save the uml png url
-    private String saveFolder =  "..\\resources\\static\\";
-    private String saveName = "temp";
-    private String saveTxtName = saveName + ".txt";
-    private String savePngName = saveName + ".png";
 
-    // q: 如何在这里初始化构造函数
-    // a: 你可以看一下这个类的构造函数
+    String openAIUrl = "https://api.openai-proxy.com/pro/chat/completions"; // 目标URL
+    String apiKey = "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";
+    private final String saveFolder =  "AIUml\\";
+    private final String saveName = "temp";
+    private final String saveTxtPath = saveFolder + saveName + ".txt";
+    private final String savePngPath = saveFolder + saveName + ".png";
+    private final String plantUmlPath = saveFolder + "plantuml.jar";
 
 
     public AIUmlGenerator() {
     }
 
     public Dictionary<String, String> askGPT(String userInput) {
-        // TODO: 询问GPT
-        JSONObject res = getGPTResponse(userInput);
-        System.out.println(res);
-        if (res.isEmpty() || res.getInteger("code") != 200) {
-            return null;
+        JSONObject res = null;
+        try {
+            res = getGPTResponse(userInput);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        String resData = res.get("data").toString();
-        return separateResponse(resData);
+        System.out.println("响应内容：" + res);
+        if (res.isEmpty() || res.getInteger("code") != 200) {
+            System.out.println("GPT error" + res);
+            return separateResponse(null);
+        }
+        // 拿出 data 进行分割
+        return separateResponse(res.get("data").toString());
     }
 
-    // TODO: 本地静态保存返回uml PNG_URL
     public String getNewUmlUrl(String umlCode) {
-//      TODO:  1. 生成UML.txt文件
         saveUmlCode(umlCode);
-        saveUmlPng();
-        return savePngName;
+        try {
+            saveUmlPng();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return savePngPath;
     }
 
 
-    private Dictionary<String, String> separateResponse(String res) {
-//        TODO: 实现分割; 通过正则表达式
+    private Dictionary<String, String> separateResponse(String resDataText) {
+        String umlCode = resDataText;
         Dictionary<String, String> dict = new Hashtable<>();
-        dict.put("gptResponse", "res");
-        dict.put("umlCode", "@startUml .... @endUml");
-        dict.put("umlIntro", "这个UML展示了...");
-        System.out.println("separateResponse: " + dict);
-//        q: 为什么这里的dict是空的
-//        a: 因为你没有返回值
-//        q: 我明明返回了值啊
-//        a: 你返回的是null
+        if (resDataText == null) {
+            dict.put("gptResponse", null);
+            dict.put("umlCode", null);
+            dict.put("umlIntro", null);
+            return dict;
+        }
+
+        // 定义正则表达式
+        String umlRegex = "`{3}.*\\n([\\s\\S]+?)\\n`{3}";
+        Pattern pattern = Pattern.compile(umlRegex);
+
+        // 查找并提取UML代码
+        Matcher matcher = pattern.matcher(resDataText);
+        if (matcher.find()) {
+            umlCode = matcher.group(1);
+        }
+
+        // 提取其他文字介绍
+        String umlIntro = resDataText.replaceAll(umlRegex, "").trim();
+
+        dict.put("gptResponse", resDataText);
+        dict.put("umlCode", umlCode);
+        dict.put("umlIntro", umlIntro);
+
         return dict;
     }
 
-    private JSONObject getGPTResponse(String userInput) {
-//        TODO: 实现GPT请求
-//        waitTime(10);
-        JSONObject res = new JSONObject();
-        res.put("code", 200);
-        res.put("data", "这是GPT的返回值");
-        return res;
+    private JSONObject getGPTResponse(String userInput) throws IOException {
+
+        // JSON数据
+//        String jsonBody = "{ \"apiKey\": \"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\", \"sessionId\": \"test\" , \"content\": \"你好你是谁\" }";
+        String defaultPrompt = ". 结合以上需求帮我写一个UML图代码demo，要符合plantUML的语法。 你的回答要包含_uml代码和uml代码描述。UML图内名称部分是中文,有关UML语法的关键词必须是英文：start,stop,if,else,-->,-[dashed]->,true,false。";
+        String jsonBody = "{ \"apiKey\": \"" + apiKey + "\", \"sessionId\": \"test\" , \"content\": \"" + userInput + defaultPrompt +"\" }";
+
+        URL obj = new URL(openAIUrl);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // 设置请求方法为POST
+        con.setRequestMethod("POST");
+
+        // 设置请求属性
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+        con.getOutputStream().write(jsonBody.getBytes(StandardCharsets.UTF_8));
+
+
+        // 读取响应数据
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return JSONObject.parseObject(response.toString());
     }
 
     private void saveUmlCode(String umlCode) {
-//        TODO: 实现保存UML代码
-        // 1. 生成UML.txt文件, 保存至本地
-        waitTime(1);
+//        通过FileStream 将这些代码保存到本地一个文件中
+
+        try {
+            File file = new File(saveTxtPath);
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(umlCode);
+            bw.close();
+            System.out.println("文件成功保存。");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void saveUmlPng() {
-//        TODO: 实现保存UML图片, 通过运行cmd命令
-//        waitTime(2);
-    }
+    private void saveUmlPng() throws IOException, InterruptedException {
+//        String command = "cmd /c copy /y AIUml\\temp.txt AIUml\\temp2.txt"; // CMD命令
+        String command = "cmd /c java " +
+                "-Djava.awt.headless=true " +
+                "-Dsun.java2d.uiScale=1.3 " +
+                "-Dsun.java2d.dpiaware=false " +
+                "-Dsun.java2d.noddraw=true " +
+                "-jar " + plantUmlPath + " " +
+                saveTxtPath; // CMD命令
+//        System.out.println("saveUmlPng: " + command);
+        Process process = Runtime.getRuntime().exec(command); // 运行CMD命令
 
-    private void runCmd(String cmd) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream())); // 获取CMD输出流
 
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line); // 输出CMD命令结果
+        }
+        int exitCode = process.waitFor(); // CMD命令执行完成后获取退出码
+        System.out.println("saveUmlPng：命令执行完成，退出码为：" + exitCode);
+        System.out.println("saveUmlPng: "+ savePngPath);
     }
 
     private void waitTime(int time) {
@@ -86,5 +164,86 @@ public class AIUmlGenerator {
             return;
         }
         System.out.println("sleep end");
+    }
+
+    public static void main(String[] args) {
+        AIUmlGenerator aiUmlGenerator = new AIUmlGenerator();
+        String userInput = "我想要一个UML, demo";
+//        String allString = "好的，以下是一个简单的示例UML图的代码演示：\\n\\n```plantuml\\n@startuml\\ntitle 示例UML图\\n\\n|用户|\\n\\nstart\\n\\nif (用户已登录？) then (yes)\\n  :显示个人资料页面;\\nelse (no)\\n  :跳转到登录页面;\\nendif\\n\\n|用户|\\n\\nstop\\n@enduml\\n```\\n\\n上述UML图中，描述了一个用户登录状态的判断过程。用户可以通过登录来访问个人资料页面，如果用户未登录，则会被跳转到登录页面。使用PlantUML语法绘制以上UML图。\\n\\n代码描述如下：\\n\\n1. `@startuml` 和 `@enduml` 命令分别表示 UML 图的开始和结束。\\n2. `title` 命令用于为 UML 图添加标题。\\n3. `|用户|` 用于表示类的名称，竖线符号表示 UML 类图中的分隔符。\\n4. `start` 命令表示 UML 图的开始节点。\\n5. `if` 命令用于表示条件语句，`then` 表示条件为真时将执行的语句块，`else` 表示条件为假时将执行的语句块。\\n6. `-->` 表示连线，表示从一个节点到另一个节点。\\n7. `stop` 命令表示 UML 图的结束节点。\\n\\n以上是一个简单的UML图的代码演示，你可以根据需要自行构建更复杂的UML图。";
+
+//        Dictionary<String, String> res = aiUmlGenerator.separateResponse(allString);
+
+        Dictionary<String, String> res = aiUmlGenerator.askGPT(userInput);
+        System.out.println("askGPT:" + res.toString());
+        System.out.println("askGPT:" + res.get("userInput"));
+        System.out.println("--------------------------------------------");
+        System.out.println("umlCode:" + res.get("umlCode"));
+        System.out.println("--------------------------------------------");
+        System.out.println("umlIntro:" + res.get("umlIntro"));
+        System.out.println("--------------------------------------------");
+
+        String path = aiUmlGenerator.getNewUmlUrl(res.get("umlCode"));
+        System.out.println("path:" + path);
+
+
+        String umlCode = "以下是商场管理系统的UML类图的代码：\n" +
+                "\n" +
+                "```plantuml\n" +
+                "@startuml\n" +
+                "title 商场管理系统类图\n" +
+                "\n" +
+                "class 商品 {\n" +
+                "  -编号: int\n" +
+                "  -名称: string\n" +
+                "  -价格: float\n" +
+                "  -库存量: int\n" +
+                "  +商品(编号:int, 名称:string, 价格:float, 库存量:int)\n" +
+                "  +获取编号():int\n" +
+                "  +获取名称():string\n" +
+                "  +获取价格():float\n" +
+                "  +获取库存量():int\n" +
+                "  +添加库存量(数量:int):void\n" +
+                "}\n" +
+                "\n" +
+                "abstract class 员工 {\n" +
+                "  -编号: int\n" +
+                "  -姓名: string\n" +
+                "  -性别: string\n" +
+                "  -年龄: int\n" +
+                "  +员工(编号:int, 姓名:string, 性别:string, 年龄:int)\n" +
+                "  +获取编号():int\n" +
+                "  +获取姓名():string\n" +
+                "  +获取性别():string\n" +
+                "  +获取年龄():int\n" +
+                "  +上班打卡():void\n" +
+                "  +下班打卡():void\n" +
+                "}\n" +
+                "\n" +
+                "class 收银员 extends 员工 {\n" +
+                "  -货币: string\n" +
+                "  +收银员(编号:int, 姓名:string, 性别:string, 年龄:int)\n" +
+                "  +收款(金额:float):void\n" +
+                "}\n" +
+                "\n" +
+                "class 管理员 extends 员工 {\n" +
+                "  +管理员(编号:int, 姓名:string, 性别:string, 年龄:int)\n" +
+                "  +进货(商品:商品, 数量:int):void\n" +
+                "  +查看库存(商品:商品):int\n" +
+                "  +修改价格(商品:商品, 新价格:float):void\n" +
+                "}\n" +
+                "\n" +
+                "class 领导 extends 员工 {\n" +
+                "  -职位: string\n" +
+                "  +领导(编号:int, 姓名:string, 性别:string, 年龄:int, 职位:string)\n" +
+                "  +发表讲话():void\n" +
+                "}\n" +
+                "\n" +
+                "商品 -- 员工\n" +
+                "员工 <|-- 收银员\n" +
+                "员工 <|-- 管理员\n" +
+                "员工 <|-- 领导\n" +
+                "@enduml\n" +
+                "```\n";
+//        aiUmlGenerator.getNewUmlUrl(umlCode);
     }
 }
